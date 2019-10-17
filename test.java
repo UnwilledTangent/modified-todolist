4cb95d9f7facdc68a7bfbafeef91c72ab8f537ca
Sean Tin <tinsean@hotmail.com>
2019-10-17 13:56:33 +0800

    Finished lesson 17 of thenewboston Java

--- test.java
+++ test.java
@@ -4,23 +4,41 @@ import java.util.Scanner;
 
 public class test {
 	public static void main(String[] args) {
-		// Lesson 14: Using Multiple Classes
+//		Lesson 14: Using Multiple Classes
+//		
+//		anytime you run a java program, it looks for main as the first thing
+//		
+//		tuna is a class
+//		tunaObject is an object
+//		tuna tunaObject = new tuna();
+//		
+//		uses the method simpleMessage() inside tunaObject
+//		tunaObject.simpleMessage();
+//		
+//		Lesson 15: Methods with Parameters
+//		Scanner input = new Scanner(System.in);
+//		tuna tunaObject = new tuna();
+//		System.out.println("Enter your name here: ");
+//		String name = input.nextLine();
+//		
+//		tunaObject.simpleMessage(name);
 		
-		// anytime you run a java program, it looks for main as the first thing
+		// Lesson 16: Many Methods and Instances
 		
-		// tuna is a class
-		// tunaObject is an object
-		//tuna tunaObject = new tuna();
+//		Scanner input = new Scanner(System.in);
+//		tuna tunaObject = new tuna();
+//		System.out.println("Enter name of first gf here: ");
+//		String temp = input.nextLine();
+//		tunaObject.setName(temp);
+//		tunaObject.saying();
 		
-		// uses the method simpleMessage() inside tunaObject
-		//tunaObject.simpleMessage();
+		// Lesson 17: Constructors
+		// Constructors allows us to initialise variables as soon as we create an object
 		
-		// Lesson 15: Methods with Parameters
-		Scanner input = new Scanner(System.in);
-		tuna tunaObject = new tuna();
-		System.out.println("Enter your name here: ");
-		String name = input.nextLine();
+		tuna tunaObject = new tuna("Pokemon");
+		tuna tunaObject2 = new tuna("Nicole");
 		
-		tunaObject.simpleMessage(name);
+		tunaObject.saying();
+		tunaObject2.saying();
 	}
 }
