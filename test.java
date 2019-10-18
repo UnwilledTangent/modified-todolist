7db5e1147860c9149044e24b8c4629038f1edbde
Sean Tin <tinsean@hotmail.com>
2019-10-18 16:10:12 +0800

    Finished Lesson 38 "this"

--- test.java
+++ test.java
@@ -4,6 +4,12 @@ import java.util.Scanner;
 
 public class test {
 	public static void main(String[] args) {
+		tuna tunaObject = new tuna();
+		System.out.println(tunaObject.toMilitary());
+		System.out.println(tunaObject.toString());
 		
+		tunaObject.setTime(13, 27, 6);
+		System.out.println(tunaObject.toMilitary());
+		System.out.println(tunaObject.toString());
 	}
 }
