ecd7b383356b4728425865f93355317c7434e9ed
Sean Tin <unwilledtangent@gmail.com>
2019-10-23 16:41:15 +0800

    End of Lesson 54 - Event Handler Program

--- test.java
+++ test.java
@@ -5,7 +5,7 @@ public class test {
 	public static void main(String[] args) {
 		tuna bucky = new tuna();
 		bucky.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		bucky.setSize(275,180);
+		bucky.setSize(350,100);
 		bucky.setVisible(true);
 	}
 }
