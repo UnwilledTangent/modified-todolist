0f9c3b3433f9dba84a17a7bb8b4915a06233da87
Sean Tin <unwilledtangent@gmail.com>
2019-10-23 16:07:08 +0800

    End of Lesson 51 - GUI with JFrame

--- test.java
+++ test.java
@@ -1,15 +1,11 @@
 package test;
-import javax.swing.JOptionPane;
+import javax.swing.JFrame;
 
 public class test {
 	public static void main(String[] args) {
-		String fn = JOptionPane.showInputDialog("Enter first number");
-		String sn = JOptionPane.showInputDialog("Enter second number");
-		
-		int num1 = Integer.parseInt(fn);
-		int num2 = Integer.parseInt(sn);
-		int sum = num1 + num2;
-		
-		JOptionPane.showMessageDialog(null, "The answer is "+ sum, "The title", JOptionPane.PLAIN_MESSAGE);
+		tuna bucky = new tuna();
+		bucky.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		bucky.setSize(275,180);
+		bucky.setVisible(true);
 	}
 }
--- tuna.java
+++ tuna.java
@@ -1,52 +1,21 @@
 package test;
+import java.awt.FlowLayout;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 
-public class tuna {
-	private int hour;
-	private int minute;
-	private int second;
+// extends means inherits all the methods and variables of JFrame
+// static means every object shares the same variable.
+// so if variable change, means variable change for all objects
+public class tuna extends JFrame{
+	private JLabel item1;
 	
 	public tuna() {
-		this(0,0,0);
-	}
-	public tuna(int h) {
-		this(h,0,0);
-	}
-	public tuna(int h, int m) {
-		this(h,m,0);
-	}
-	public tuna(int h, int m, int s) {
-		setTime(h,m,s);
-	}
-	public void setTime(int h, int m, int s) {
-		setHour(h);
-		setMinute(m);
-		setSecond(s);
-	}
-	public void setHour(int h) {
-		hour = ((h>0&&h<24)?h:0);
-	}
-	// same as 
-	// if(h>0&&h<24)
-	//     hour = h
-	// else
-	//     hour = 0
-	public void setMinute(int m) {
-		minute = ((m>0&&m<60)?m:0);
-	}
-	public void setSecond(int s) {
-		second = ((s>0&&s<60)?s:0);
-	}
-	public int getHour() {
-		return hour;
-	}
-	public int getMinute() {
-		return minute;
-	}
-	public int getSecond() {
-		return second;
-	}
-	public String toMilitary() {
-		// means that the hour minute and seconds has 2 decimal places
-		return String.format("%02d:%02d:%02d", getHour(), getMinute(), getSecond());
+		super("The title bar");
+		setLayout(new FlowLayout());
+		
+		item1 = new JLabel("This is a sentence");
+		// setToolTipText shows something up on hover
+		item1.setToolTipText("This is gonna show up on hover");
+		add(item1);
 	}
 }
