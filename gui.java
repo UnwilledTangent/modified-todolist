6376717b22c890c00b70fa08bb8eb18f4dceca11
Sean Tin <unwilledtangent@gmail.com>
2019-10-24 10:55:54 +0800

    End of Lesson 76 - Mouse Events

--- gui.java
+++ gui.java
@@ -2,36 +2,56 @@ package test;
 
 import java.awt.*;
 import java.awt.event.*;
+import java.util.logging.Handler;
 
 import javax.swing.*;
 
 public class gui extends JFrame {
-	private JComboBox box;
-	private JLabel picture;
-	
-	private static String[] filename = {"b.png","x.jpg"};
-	private Icon[] pics = {new ImageIcon(getClass().getResource(filename[0])), new ImageIcon(getClass().getResource(filename[1]))};
+	private JPanel mousePanel;
+	private JLabel statusbar;
 	
 	public gui() {
-		super("The Title");
-		setLayout(new FlowLayout());
+		super("My title");
 		
-		// automatically puts filename list in a combo box
-		box = new JComboBox(filename);
+		mousePanel = new JPanel();
+		mousePanel.setBackground(Color.WHITE);
+		add(mousePanel, BorderLayout.CENTER);
 		
-		box.addItemListener(
-				new ItemListener() {
-					@Override
-					public void itemStateChanged(ItemEvent event) {
-						// Whatever you select
-						if(event.getStateChange()==ItemEvent.SELECTED)
-							picture.setIcon(pics[box.getSelectedIndex()]);
-					}
-				}
-		);
+		statusbar = new JLabel("Default");
+		add(statusbar, BorderLayout.SOUTH);
 		
-		add(box);
-		picture=new JLabel(pics[0]);
-		add(picture);
+		HandlerClass handler = new HandlerClass();
+		mousePanel.addMouseListener(handler);
+		mousePanel.addMouseMotionListener(handler);
+	}
+	
+	private class HandlerClass implements MouseListener, MouseMotionListener{
+		public void mouseClicked(MouseEvent event) {
+			statusbar.setText(String.format("Clicked at %d,%d", event.getX(),event.getY()));
+		}
+		public void mousePressed(MouseEvent event) {
+			statusbar.setText("You pressed down the mouse");
+		}
+		public void mouseReleased(MouseEvent event) {
+			statusbar.setText("You released the button");
+		}
+		public void mouseEntered(MouseEvent event) {
+			statusbar.setText("You entered the area");
+			mousePanel.setBackground(Color.RED);
+		}
+		public void mouseExited(MouseEvent event) {
+			statusbar.setText("The mouse has left the window");
+			mousePanel.setBackground(Color.WHITE);
+		}
+		
+		// these are mouse motion event
+		@Override
+		public void mouseDragged(MouseEvent event) {
+			statusbar.setText("You are dragging the mouse");
+		}
+		@Override
+		public void mouseMoved(MouseEvent e) {
+			statusbar.setText("You moved the mouse");
+		}
 	}
 }
