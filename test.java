25fef0662e22d72ac7d3f6e6f020cb725dead1b5
Sean Tin <unwilledtangent@gmail.com>
2019-10-24 11:01:55 +0800

    Start of creation of todolist

--- test.java
+++ test.java
@@ -1,11 +1,7 @@
 package test;
-import javax.swing.JFrame;
 
 public class test {
 	public static void main(String[] args) {
-		gui go = new gui();
-		go.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		go.setSize(300,200);
-		go.setVisible(true);
+		System.out.println("Hello World!");
 	}
 }
